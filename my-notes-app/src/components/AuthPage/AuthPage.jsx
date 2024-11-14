dc67da8c68d293ea232d1fe5766efffb14c67b5c
Ashton Gasser <ashton@pepper.me>
2024-11-14 15:46:38 -0600

    useStates created for AuthPage component. ToggleForm set for AuthePage

--- my-notes-app/src/components/AuthPage/AuthPage.jsx
+++ my-notes-app/src/components/AuthPage/AuthPage.jsx
@@ -1,8 +1,19 @@
+import React, { useState } from 'react';
+//import { Box, Button, TextField, Typography } from '@mui/material'
 
 
+export default function AuthPage({onLogin}) {
+    const [isLogin, setIsLogin] = useState(true);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
 
-export default function AuthPage() {
+    const toggleForm = () => {
+        setIsLogin(!isLogin);
+        setMessage('');
+        setEmail('');
+        setPassword('');
+    };
 
 
-    
 }
